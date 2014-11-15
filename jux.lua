43c77ad5f5dc559666672706926f12eb8cb4ab8e
Arkiver2 <Arkiver@hotmail.com>
2014-11-15 15:56:40 +0100

    jux.lua

--- jux.lua
+++ jux.lua
@@ -80,7 +80,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       html = read_file(file)
       
       
-      if string.match(url, item_value.."%.jux%.com/[0-9]+[^/%.]") then
+      if string.match(url, item_value.."%.jux%.com/[0-9]+[^/]") and not string.match(url, "%.json") then
         local newurl = url..".json"
         if downloaded[newurl] ~= true and addedtolist[newurl] ~= true then
           table.insert(urls, { url=newurl })
