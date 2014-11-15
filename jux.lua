8c1f55d3dbdbb62f7c23920d421ea03bb8fd784d
Arkiver2 <Arkiver@hotmail.com>
2014-11-15 15:50:29 +0100

    jux.lua: fix .json.json.json loop

--- jux.lua
+++ jux.lua
@@ -80,7 +80,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       html = read_file(file)
       
       
-      if string.match(url, item_value.."%.jux%.com/[0-9]+[^/]") then
+      if string.match(url, item_value.."%.jux%.com/[0-9]+[^/%.]") then
         local newurl = url..".json"
         if downloaded[newurl] ~= true and addedtolist[newurl] ~= true then
           table.insert(urls, { url=newurl })
@@ -96,7 +96,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
         if downloaded[newurl] ~= true and addedtolist[newurl] ~= true then
           table.insert(urls, { url=newurl })
           addedtolist[newurl] = true
-          io.stdout:write("Url "..customurl.." 2.  \n")
+          io.stdout:write("Url "..newurl.." 2.  \n")
           io.stdout:flush()
         end
       end
